09d315aca6765cedfe292945fd4493c90eaef57c
c-livi <121881563+c-livi@users.noreply.github.com>
2023-01-05 11:27:14 -0500

    Update logger.rs

--- logger.rs
+++ logger.rs
@@ -1,9 +1,14 @@
 use std::io::{self, Read, Write};
 use std::net::{TcpStream, TcpListener};
 use std::{thread, time};
+use ini::Ini;
 
 fn main() {
-    let mut stream = TcpStream::connect("parent_computer_ip:8080").unwrap();
+    
+    let config = Ini::load_from_file("config.ini").unwrap();
+    let parent_ip = config.section(Some("Parent")).unwrap().get("IP").unwrap();
+    
+    let mut stream = TcpStream::connect(format!("{}:8080", parent_ip)).unwrap();
 
     loop {
         
