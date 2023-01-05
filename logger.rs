d00c70d0f615afff478a6047795a4cc73b6c34cb
c-livi <121881563+c-livi@users.noreply.github.com>
2023-01-05 10:30:44 -0500

    Update logger.rs

--- logger.rs
+++ logger.rs
@@ -7,28 +7,23 @@ fn main() {
         let mut input = String::new();
         let mut output = String::new();
 
-        // Read input from the keyboard
         io::stdin().read_line(&mut input).unwrap();
-
-        // Record the input to the buffer
+        
         buffer.push_str(&input);
-
-        // Execute the input as a command
-        let mut cmd = Command::new("bash");
-        cmd.arg("-c").arg(&input);
-        let output = cmd.output().unwrap();
-
-        // Write the output of the command to the screen
+        
         io::stdout().write_all(&output.stdout).unwrap();
         io::stderr().write_all(&output.stderr).unwrap();
 
-        // Check if the command was "exit"
-        if input.trim() == "exit" {
+        let os_status = Command::new("systemctl")
+            .arg("is-system-running")
+            .output()
+            .unwrap();
+
+        if !os_status.status.success() {
             break;
         }
     }
 
-    // Write the logged keystrokes to a file
     let username = env::var("USER").unwrap();
     let mut file = String::new();
     file.push_str("/home/");
